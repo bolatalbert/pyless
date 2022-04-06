b74e43ec97cd5b879c88fb6d615d5d867790fdad
bolatalbert <bolatalbert@gmail.com>
2022-04-06 14:11:07 +0300

    lesson_1-1.py changed

--- lesson_1-1.py
+++ lesson_1-1.py
@@ -1,1 +1,11 @@
 print ('my first python file')
+# выполняю пример с паролем
+original_password = "delta12345"
+password = input('Введите пароль с учетом регистра: ')
+access = False #переменная, хранит разрешение на доступ - разобраться!
+if password == original_password:
+    print ('Четовски верный пароль!')
+    access = True
+if password != original_password:
+    print ('думай дальше - ты ввел неверный пароль')
+
